1bf615ff5c7f588af9097d5475a971645bf338cf
Franc Cosp <franccospmeyaeso@gmail.com>
2021-05-13 18:15:58 +0000

    copiado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,13 +1,11 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import ToDoList from "./ToDoList";
 
 //create your first component
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-		toDolist
+			<ToDoList />
 		</div>
 	);
 }
